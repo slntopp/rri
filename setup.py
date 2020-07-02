14dd741afb7a2e3e97f768edab46f72d3b043440
Mikita Iwanowski <info@slnt-opp.xyz>
2020-07-02 19:31:04 +0200

    pip setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simple-rri',
+    version='0.1',
+    description='Simple Recursive Requirements Installer',
+    url='https://github.com/slntopp/simple-rri',
+    author='slntopp',
+    author_email='info@slnt-opp.xyz',
+    license='Apache-2.0 License',
+    packages=['srri'],
+    zip_safe=False)
